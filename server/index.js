c4a3fbe3ebf760ae8516280c6a8db81efb37258b
azproduction <azazel.private@gmail.com>
2013-11-18 19:04:20 +0600

    server interface fix

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 var express = require('express'),
     router = require('./router'),
     http = require('http');
- 
-var app = express();
- 
-app.configure(function () {
-    app.use(express.logger('dev'));
-});
- 
-app.configure('development', function(){
-    app.use(express.errorHandler());
-});
 
-router(app);
+module.exports = function () {
+    var app = express();
 
-module.exports = app;
+    app.configure(function () {
+        app.use(express.logger('dev'));
+    });
+
+    app.configure('development', function(){
+        app.use(express.errorHandler());
+    });
+
+    router(app);
+    return app;
+};
