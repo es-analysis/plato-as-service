94cb30b79d879331919b791e6a42c10d7e6771b1
azproduction <azazel.private@gmail.com>
2013-11-18 20:44:48 +0600

    reportSettings update

--- server/router.js
+++ server/router.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.get('/', indexController.usage);
     app.get('/:user/:repo/', indexController.redirectToMaster);
     app.get('/:user/:repo/:branch/', indexController.index);
-    app.get('*', express.static(__dirname + '/../reports'));
+    app.get('*', express.static(app.locals.reportSettings.reports || __dirname + '/../reports'));
     app.get('*', function (req, res) {
         res.send(404);
     });
